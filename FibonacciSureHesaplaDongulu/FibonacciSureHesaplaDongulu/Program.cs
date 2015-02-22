6dc5e4f4bec3a01511b76329416f371d9a215b2f
YildirimSahiner <yshner@gmail.com>
2015-02-22 14:58:49 +0200

    Fibonacci Serisi Loop-Recursive
    Algoritma Analizi Ödevi - 1

---
+++ FibonacciSureHesaplaDongulu/FibonacciSureHesaplaDongulu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSureHesaplaDongulu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch(); 
+            Console.WriteLine("Hangi sayının Fibonaccisi Lazım:");          
+            double sayi = Convert.ToDouble(Console.ReadLine()); stopwatch.Start();            
+            Console.WriteLine("Fibonacci değeri:" + Fibo_dongu(sayi));
+            stopwatch.Stop();
+            TimeSpan zaman = stopwatch.Elapsed; 
+            string elapsedTime = String.Format("{0} saat + {1} dakika + {2} saniye + {3} salise ", zaman.Hours, zaman.Minutes, zaman.Seconds, zaman.TotalMilliseconds*10000);            
+            Console.WriteLine("Toplam Zaman:" + elapsedTime +"geçti ");
+            Console.ReadKey(); 
+ }
+
+        public static double Fibo_dongu(double fibonacci)
+        {
+            double degisken1 = 0; double degisken2 = 1;           
+            for (int i = 0; i < fibonacci; i++)
+            {
+                double gecici = degisken1; degisken1 = degisken2;
+                degisken2 = gecici + degisken2;
+            }
+            return degisken1;
+        }
+        
+    }
+}
---
+++ FibonacciSureHesaplama/FibonacciSureHesaplama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSureHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            Console.WriteLine("Hangi Sayının Fibonaccisi Lazım?");
+            double sayi = Convert.ToDouble(Console.ReadLine());            
+            stopwatch.Start();
+            Console.WriteLine("Fibonacci:" + Fib_Recursion(sayi));
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed; 
+            string elapsedTime = String.Format("{0} saat + {1} dakika + {2} saniye + {3} salise ",ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds*10000);
+            Console.WriteLine("Toplam Zaman:" + elapsedTime +" geçti.");
+            Console.ReadKey();
+        
+        }
+        public static double Fib_Recursion(double sayi)
+        {
+            if (sayi <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+            return Fib_Recursion(sayi-2) + Fib_Recursion(sayi-1);
+            }
+       
+        }
+    }
+}
